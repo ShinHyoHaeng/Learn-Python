4cb27c429403188db8c6fdca98f104986605a0e7
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-07 19:44:51 +0900

    2021.08.26 개인 학습

---
+++ 20210826_Python/05B-count.py
@@ -0,0 +1,9 @@
+## range
+
+print("[0-4]")
+for x in range(5): # range(5)로 0,1,2,3,4까지 5번 반복
+	print(x) # 변수 x 값 출력
+	
+print("[1-10]")
+for x in range(1,11): #1,2,...,10까지 10번 반복(11은 제외)
+	print(x) # 변수 x 값 출력	
