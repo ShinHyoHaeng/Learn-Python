a1527a249050e23329e0e4d281fba1674a69c682
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-13 13:51:09 +0900

    Chapter 09. 함수: 모듈

---
+++ Ch09_Function/09-04-01-func.py
@@ -0,0 +1,12 @@
+##
+
+def func1():
+    print("func.py의 func1()이 호출됨")
+
+
+def func2():
+    print("func.py의 func2()이 호출됨")
+
+
+def func3():
+    print("func.py의 func3()이 호출됨")
---
+++ Ch09_Function/09-04-02-func-import.py
@@ -0,0 +1,12 @@
+import func
+
+func.func1()
+func.func2()
+func.func3()
+
+
+import sys
+print(sys.builtin_module_names)
+
+import math
+dir(math)
