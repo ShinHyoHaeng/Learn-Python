a9e699c5fdbff906f2553256e18abdd57834802c
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-13 15:30:28 +0900

    Chapter 12. 윈도우 프로그램: 키보드/마우스 이벤트(마우스 이벤트 응용)
    - 지정된 위젯을 클릭했을 때 함수 호출

---
+++ Ch12_Window-Program/12-14-img-click-event.py
@@ -0,0 +1,25 @@
+
+## 키보드/마우스 이벤트: 지정된 위젯을 클릭했을 때 함수 호출
+
+from tkinter import *
+from tkinter import messagebox
+
+# 함수 정의 부분
+# 마우스 이벤트 발생 시 실행할 함수 정의
+def clickImage(event) :
+    messagebox.showinfo("마우스", "토끼 이미지에서 클릭 이벤트 실행")
+
+# 메인 코드 부분
+window = Tk()
+window.geometry("400x400")
+
+# 이미지 준비
+photo = PhotoImage(file="gif/rabbit.gif")
+label = Label(window, image=photo)
+
+# 이미지를 넣은 라벨에 마우스 클릭 시 clickImage() 함수 실행
+label.bind("<Button>", clickImage)
+
+label.pack(expand=1, anchor=CENTER)
+
+window.mainloop()
