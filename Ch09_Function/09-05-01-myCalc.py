119fb649d247180b1b36f5d60a52d9e6326c8136
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-13 13:51:42 +0900

    Chapter 09. 함수: 모듈 실습(계산기)

---
+++ Ch09_Function/09-05-01-myCalc.py
@@ -0,0 +1,16 @@
+
+def plus(num1, num2):
+    return num1+num2
+
+def minus(num1, num2):
+    return num1-num2
+
+def multiply(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    if num2 != 0 :
+        return num1/num2
+    else :
+        print("0으로는 나눌 수 없습니다")
+        return -1
