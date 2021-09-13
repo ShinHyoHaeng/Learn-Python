6ea455e2c7a2a9f894ac0f95c93fa17c355f4286
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-13 13:27:24 +0900

    파일 삭제
    - 예제 파일을 명확하게 알아보도록 재정렬 필요
    - 수업날짜 대신 챕터별로 변경

--- 20210826_Python/03B-tri.py
+++
@@ -1,9 +0,0 @@
-import turtle as t
-
-d = 100 # 변수 d에 값 100을 저장(d값을 바꾸면 삼각형 모양도 바뀜)
-
-# 삼각형 그리기
-for x in range(3):
-	t.forward(d)
-	t.left(120)
-
--- 20210826_Python/04A-hello.py
+++
@@ -1,5 +0,0 @@
-## for문
-
-# for x in range(반복 횟수)
-for x in range(10): # 10번 반복
-	print("Hello")
